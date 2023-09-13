fe920e180c1e4d382cbcd958b1c02d619ed9088f
qiuxinping <qiuxinping@bytedance.com>
2023-09-13 21:30:23 +0800

    一次批量5000条

--- src/components/DiffCard/index.tsx
+++ src/components/DiffCard/index.tsx
@@ -6,7 +6,7 @@ import { icons } from '../../icons'
 import { useTranslation } from 'react-i18next';
 
 
-
+const pageSize = 100
 
 
 export default function DiffCard(
@@ -23,6 +23,7 @@ export default function DiffCard(
   /** 已经设置成功的下标 */
   const settledIndex = useRef<Set<number>>(new Set([-1]));
   const { t } = useTranslation();
+  const [page, setPage] = useState(0)
   useEffect(() => {
     if (props.successed.length) {
       toSetList.forEach((list, index) => {
@@ -39,7 +40,7 @@ export default function DiffCard(
     <div className="d">
       <div className="fieldTitle">
         {/* @ts-ignore */}
-        {icons[fieldMeta.type]} {fieldMeta.name}
+        {icons[fieldMeta.type]} {fieldMeta.name} {`(${toSetList.length})`}
       </div>
       <div className="lists">
         <div className="header">
@@ -48,7 +49,7 @@ export default function DiffCard(
           <div className="operation">{t("opt")}</div>
         </div>
         <div>
-          {toSetList.map((list, toSetListIndex) => {
+          {toSetList.slice(page, (page + 1) * pageSize).map((list, toSetListIndex) => {
             const info = replaceInfo.filter((r) => r.recordId === list.recordId);
             const { oldCellValue } = info[0];
             const { recordId, value: newCellValue } = list;
@@ -148,6 +149,15 @@ export default function DiffCard(
             );
           })}
         </div>
+        {(page + 1) * pageSize < toSetList?.length && toSetList.length > pageSize && <div
+        onClick={()=>setPage(page+1)}
+        className='loadMore'>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.638 13.35 12 18.988l-5.943-5.943-.84-.835a1 1 0 1 0-1.409 1.418l.667.663 6.818 6.819a1 1 0 0 0 1.414 0l6.473-6.473 1.013-1.011a.999.999 0 0 0-1.41-1.416l-1.146 1.142Z" fill="#2B2F36" />
+            <path d="M17.638 4.443 12 10.08 6.058 4.136l-.84-.835A1 1 0 1 0 3.81 4.72l.667.663 6.818 6.818a1 1 0 0 0 1.414 0l6.473-6.473 1.013-1.011a.999.999 0 0 0-1.41-1.416l-1.146 1.142Z" fill="#2B2F36" />
+          </svg>
+
+        </div>}
       </div>
     </div>
   );
